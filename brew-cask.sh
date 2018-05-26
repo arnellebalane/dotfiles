b0378e272166a2b8fa8d85ac2a52cc9825c246eb
Arnelle Balane <arnellebalane@gmail.com>
2018-05-26 14:48:13 +0800

    Add install scripts for brew and brew cask packages

---
+++ brew-cask.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# -----------------------------------------------
+# Install desktop apps with brew-cask
+# -----------------------------------------------
+
+# Dev packages
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install visual-studio-code
+brew cask install atom
+brew cask install atom-beta
+
+# Browsers
+brew cask install google-chrome-canary
+brew cask install google-chrome
+brew cask install firefox
+brew cask install firefox-developer-edition
+brew cask install chromium
+
+# Communications
+brew cask install slack
+brew cask install discord
+brew cask install skype
+brew cask install rambox
+
+# Media
+brew cask install spotify
+brew cask install vlc
+
+# Other useful applications
+brew cask install alfred
+brew cask install notion
+brew cask install typora
+brew cask install transmission
+brew cask install figma
+
+brew csak install postman
+brew cask install tableplus
+brew cask install daisydisk
+
+brew cask install docker
+brew cask install virtualbox
+brew cask install wireshark
+
+
+
+# -----------------------------------------------
+# Remove oudated package versions
+# -----------------------------------------------
+
+brew cask cleanup
---
+++ brew.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# -----------------------------------------------
+# Install Homebrew
+# -----------------------------------------------
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we're using the latest Homebrew
+brew update
+
+# Upgrade already-installed formulae
+brew upgrade
+
+
+
+# -----------------------------------------------
+# Install Homebrew packages
+# Based on https://github.com/paulirish/dotfiles/blob/master/brew.sh
+# -----------------------------------------------
+
+# GNU core utilities (those that come with macOS are outdated)
+brew install coreutils
+brew install moreutils
+
+# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+
+# GNU `sed`, overwriting the built-in `sed`
+brew install gnu-sed --with-default-names
+
+# Install wget with IRI support
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools
+brew install vim --with-override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+
+# zsh
+brew install zsh
+brew install z
+
+# Git
+brew install git
+brew install git-lfs
+
+brew install hub
+brew install tig
+
+# Databases
+brew install postgresql
+brew install redis
+brew install mongodb
+
+# Other usefil binaries
+brew install imagemagick --with-webp
+brew install tree
+brew install ncdu   # analyzes disk usage
+brew install entr   # runs arbitrary commands when files change
+
+brew install httpie
+brew install simplehttp2server
+brew install youtube-dl
+
+brew install nginx
+
+
+
+# -----------------------------------------------
+# Remove oudated package versions
+# -----------------------------------------------
+
+brew cleanup
