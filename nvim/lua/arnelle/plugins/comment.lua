9e63c1cbd438f4d62fd9681e63c60af34be1ca42
Arnelle Balane <arnellebalane@gmail.com>
2026-06-30 22:35:40 -0700

    Update nvim commentstring config

--- nvim/lua/arnelle/plugins/comment.lua
+++ nvim/lua/arnelle/plugins/comment.lua
@@ -6,6 +6,10 @@ return {
         local comment = require('Comment')
         local ts_context_commentstring = require('ts_context_commentstring.integrations.comment_nvim')
 
+        require('ts_context_commentstring').setup({
+            enable_autocmd = false,
+        })
+
         comment.setup({
             pre_hook = ts_context_commentstring.create_pre_hook(),
         })
