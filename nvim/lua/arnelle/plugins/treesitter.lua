3a9e58437a253743b1dae7cc82b7329ea3c0364e
Arnelle Balane <arnellebalane@gmail.com>
2026-09-04 10:40:21 +0800

    Patch nvim-treesitter to support latest nvim version

--- nvim/lua/arnelle/plugins/treesitter.lua
+++ nvim/lua/arnelle/plugins/treesitter.lua
@@ -50,6 +50,81 @@ return {
             },
         })
 
+        -- Neovim 0.12 changed query match[id] from a single TSNode to a list
+        -- of TSNodes, but nvim-treesitter's query_predicates.lua still treats
+        -- it as a single node in these directives, crashing the highlighter
+        -- with "attempt to call method 'range' (a nil value)" (e.g. on any
+        -- fenced markdown code block with a language, like ```lua).
+        -- Upstream: https://github.com/neovim/neovim/issues/39032
+        --           https://github.com/nvim-treesitter/nvim-treesitter/issues/8636
+        -- Remove once upstream fixes it.
+        do
+            local ts_query = require('vim.treesitter.query')
+            local html_script_type_languages = {
+                importmap = 'json',
+                module = 'javascript',
+                ['application/ecmascript'] = 'javascript',
+                ['text/ecmascript'] = 'javascript',
+            }
+            local non_filetype_match_injection_language_aliases = {
+                ex = 'elixir',
+                pl = 'perl',
+                sh = 'bash',
+                uxn = 'uxntal',
+                ts = 'typescript',
+            }
+
+            local function get_node(match, id)
+                local val = match[id]
+                if type(val) == 'table' then
+                    return val[1]
+                end
+                return val
+            end
+
+            local function get_parser_from_markdown_info_string(injection_alias)
+                local match = vim.filetype.match({ filename = 'a.' .. injection_alias })
+                return match or non_filetype_match_injection_language_aliases[injection_alias] or injection_alias
+            end
+
+            ts_query.add_directive('set-lang-from-mimetype!', function(match, _, bufnr, pred, metadata)
+                local node = get_node(match, pred[2])
+                if not node then
+                    return
+                end
+                local type_attr_value = vim.treesitter.get_node_text(node, bufnr)
+                local configured = html_script_type_languages[type_attr_value]
+                if configured then
+                    metadata['injection.language'] = configured
+                else
+                    local parts = vim.split(type_attr_value, '/', {})
+                    metadata['injection.language'] = parts[#parts]
+                end
+            end, { force = true })
+
+            ts_query.add_directive('set-lang-from-info-string!', function(match, _, bufnr, pred, metadata)
+                local node = get_node(match, pred[2])
+                if not node then
+                    return
+                end
+                local injection_alias = vim.treesitter.get_node_text(node, bufnr):lower()
+                metadata['injection.language'] = get_parser_from_markdown_info_string(injection_alias)
+            end, { force = true })
+
+            ts_query.add_directive('downcase!', function(match, _, bufnr, pred, metadata)
+                local id = pred[2]
+                local node = get_node(match, id)
+                if not node then
+                    return
+                end
+                local text = vim.treesitter.get_node_text(node, bufnr, { metadata = metadata[id] }) or ''
+                if not metadata[id] then
+                    metadata[id] = {}
+                end
+                metadata[id].text = string.lower(text)
+            end, { force = true })
+        end
+
         -- set keymaps
         vim.keymap.set('n', '<C-e>', function()
             -- local result = vim.treesitter.get_captures_at_cursor(0)
